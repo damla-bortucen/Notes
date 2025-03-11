5ece0b41d146a54342c8fdfae2e517a54d821d72
Damla Bortucen <damla.bortucen.24@ucl.ac.uk>
2025-03-11 22:06:20 +0000

    Display and view notes in categories

---
+++ src/main/java/ucl/ac/uk/servlets/DisplayCategoryServlet.java
@@ -0,0 +1,37 @@
+package ucl.ac.uk.servlets;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import ucl.ac.uk.model.Model;
+import ucl.ac.uk.model.ModelFactory;
+
+import java.io.IOException;
+import java.util.Set;
+
+@WebServlet("/displayCategory")
+public class DisplayCategoryServlet extends HttpServlet
+{
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        String category = request.getParameter("category");
+
+        if (category != null) {
+            Model model = ModelFactory.getModel();
+
+            Set<String> notesInCat = model.getNoteIdsByCategory(category);
+
+            request.setAttribute("noteIds", notesInCat);
+            request.setAttribute("category", category);
+
+            ServletContext context = getServletContext();
+            RequestDispatcher dispatch = context.getRequestDispatcher("/displayCategory.jsp");
+            dispatch.forward(request, response);
+        }
+    }
+}
